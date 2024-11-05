c8b58908ebe05dee3d93214c29b51e8b70a0ff0b
NuttavichBig <nuttavich.si@gmail.com>
2024-11-05 13:06:51 +0700

    conflict fix 2

--- src/pages/Admin/HomeAdmin.jsx
+++ src/pages/Admin/HomeAdmin.jsx
@@ -5,20 +5,6 @@ import { Outlet } from "react-router-dom";
 
 export default function HomeAdmin() {
   return (
-<<<<<<< HEAD
-    <>
-    {/* BG was not full */}
-      <div className="h-screen w-full relative bg-[#FEF9F2] flex justify-center items-start">
-        <div className="container mx-auto p-6 grid gap-5 h-full">
-          <NavbarAdmin />
-          <div className="grid grid-cols-3 gap-6 h-full">
-            <div className="col-span-1">
-              <SidebarAdmin />
-            </div>
-            <div className="col-span-2 h-full">
-              <Outlet />
-            </div>
-=======
     <div className="h-screen bg-[#FEF9F2] flex justify-center items-start">
       <div className="container mx-auto p-6 grid gap-5 bg-white h-full w-full">
         <NavbarAdmin />
@@ -28,7 +14,6 @@ export default function HomeAdmin() {
           </div>
           <div className="col-span-2 h-full bg-white p-6 rounded-lg shadow-md">
             <Outlet />
->>>>>>> dev
           </div>
         </div>
       </div>
