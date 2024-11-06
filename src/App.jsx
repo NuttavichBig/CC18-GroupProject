0ee9ae4185740221c2fcff89fa75862280624cb3
nitileo <nitipong.leo@gmail.com>
2024-11-06 13:04:31 +0700

    add feature google login front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import AppRouter from "./route/AppRouter";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
   return (
-
-    <AppRouter />
-
-
-  )
+    <GoogleOAuthProvider clientId = "566477429345-tl0j5fcllt4kbm1tmbesjrplglsd4r8q.apps.googleusercontent.com">
+      <AppRouter />
+    </GoogleOAuthProvider>
+  );
 }
