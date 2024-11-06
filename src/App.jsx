fa42337286032d13ccbb4b8d5973f00770c6e5af
nitileo <nitipong.leo@gmail.com>
2024-11-06 13:11:21 +0700

    TEST DELETE

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
   return (
-    <GoogleOAuthProvider clientId = "">
+    <GoogleOAuthProvider clientId = "566477429345-tl0j5fcllt4kbm1tmbesjrplglsd4r8q.apps.googleusercontent.com">
       <AppRouter />
     </GoogleOAuthProvider>
   );
