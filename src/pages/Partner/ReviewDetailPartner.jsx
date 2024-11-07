c121054a3eb0a4bfd32f49fb2f30e5115b019f1e
KonkamonF <konkamonfungsuk@gmail.com>
2024-11-07 11:09:16 +0700

    update partner

--- src/pages/Partner/ReviewDetailPartner.jsx
+++ src/pages/Partner/ReviewDetailPartner.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import ReviewPartner from "../../Components/Partner/ReviewPartner";
 
 export default function ReviewDetailPartner() {
+  const [reviewsPartner, setReviewsPartner] = useState(false);
   return (
     <>
+      {reviewsPartner && (
+        <ReviewPartner setReviewsPartner={setReviewsPartner} />
+      )}
       <div className="w-full text-[#543310]">
         <p className="bg-[#AF8F6F] text-3xl font-bold rounded-lg p-2 text-center  shadow-lg">
           REVIEWS
@@ -13,11 +18,9 @@ export default function ReviewDetailPartner() {
               <th className="border-collapse border p-2">ID</th>
               <th className="border-collapse border p-2">CONTENT</th>
               <th className="border-collapse border p-2">USERNAME</th>
-              <th className="border-collapse border p-2">HOTEL NAME</th>
               <th className="border-collapse border p-2">BOOKING NO.</th>
               <th className="border-collapse border p-2">RATING</th>
               <th className="border-collapse border p-2">MORE IMAGE</th>
-              <th className="border-collapse border p-2">DELETE</th>
             </tr>
           </thead>
           <tbody>
@@ -27,23 +30,15 @@ export default function ReviewDetailPartner() {
               <td className="border-collapse border p-2"></td>
               <td className="border-collapse border p-2"></td>
               <td className="border-collapse border p-2"></td>
-              <td className="border-collapse border p-2"></td>
               <td className="border-collapse border p-2">
                 <button
                   onClick={() => {
-                    setReviewDetail(true);
+                    setReviewsPartner(true);
                   }}
                   className="p-1 rounded-lg border-2 bg-[#F8F4E1] border-[#543310] text-[#543310] font-semibold shadow-lg hover:bg-[#543310] hover:text-white transition-all duration-100 ease-in-out"
                 >
                   MORE IMAGE
                 </button>
-              </td>
-              <td className="border-collapse border p-2">
-                <div className="flex justify-center">
-                  <button className=" rounded-lg p-1 border-2 border-[#CD1818] bg-[#F8F4E1] text-[#CD1818] font-semibold shadow-lg hover:bg-[#CD1818] hover:text-white transition-all duration-100 ease-in-out">
-                    Remove
-                  </button>
-                </div>
               </td>
             </tr>
           </tbody>
