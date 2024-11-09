0f86ca9516c78f2ccbb792e67a4972273a01bb59
bellgushbell <gushbellpiriyapong@gmail.com>
2024-11-09 20:04:29 +0700

    edit hp

--- src/Components/BannerSlide/HomePageSlider.jsx
+++ src/Components/BannerSlide/HomePageSlider.jsx
@@ -76,7 +76,7 @@ const HomePageSlider = () => {
           left: 0,
           width: "100%",
           height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.5)", // สีดำโปร่งแสง
+          backgroundColor: "rgba(0, 0, 0, 0.4)", // สีดำโปร่งแสง
           zIndex: 0,
         }}
       ></div>
