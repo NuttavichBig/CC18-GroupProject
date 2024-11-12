99609411673f23b369af4ba0d34b3b6f17f108c9
KonkamonF <konkamonfungsuk@gmail.com>
2024-11-12 22:11:10 +0700

    successja

--- src/Components/BookingProgrssBall/BookingProgressBallRight.jsx
+++ src/Components/BookingProgrssBall/BookingProgressBallRight.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 function BookingProgressBallRight() {
   return (
     <div className="grid grid-cols-2 items-center mb-8">
-      <h2 className="text-5xl font-bold ml-2 text-green-500">SUCCESS</h2>
+      <h2 className="text-5xl font-bold ml-2 text-green-500">PAID SUCCESS</h2>
       <div className="flex items-center h-[100px]">
         <motion.div
           className="rounded-full bg-orange-500"
