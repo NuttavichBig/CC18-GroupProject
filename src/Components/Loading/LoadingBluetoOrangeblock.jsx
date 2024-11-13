0c21916b261928b00da3b598205fb274ed78b776
bellgushbell <gushbellpiriyapong@gmail.com>
2024-11-13 20:44:56 +0700

    add loading

---
+++ src/Components/Loading/LoadingBluetoOrangeblock.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import loadingAnimationUrl from '../../assets/Loading/LoadingBlockBlueToOrange.gif';
+
+
+const LoadingBluetoOrangeblock = () => {
+    return (
+        <div style={styles.container}>
+            <img src={loadingAnimationUrl} alt="Loading..." style={styles.image} />
+
+        </div>
+    );
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100px', // ความสูง
+        width: '100px',  // ความกว้าง
+        textAlign: 'center',
+        backgroundColor: 'rgba(255, 255, 255, 0)', // พื้นหลังโปร่งแสง
+    },
+    image: {
+        width: '100px', // ขนาดภาพ
+        height: '100px', // ขนาดภาพ
+        marginBottom: '10px', // เพิ่มระยะห่างด้านล่างของภาพ
+    },
+    loadingText: {
+        fontSize: '24px', // ขนาดตัวอักษร
+        color: '#FF6AD3', // สีตัวอักษร
+        fontWeight: 'bold', // ตัวหนา
+        animation: 'fadeIn 1s ease-in-out',
+    },
+    // '@keyframes fadeIn': {
+    //     '0%': { opacity: 0 },
+    //     '100%': { opacity: 1 },
+    // },
+};
+
+
+
+export default LoadingBluetoOrangeblock;
