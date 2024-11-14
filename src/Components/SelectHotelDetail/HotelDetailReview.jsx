936d01846d990e55af515950c0c9367c4267fb6b
bellgushbell <gushbellpiriyapong@gmail.com>
2024-11-14 13:46:42 +0700

    xxx

--- src/Components/SelectHotelDetail/HotelDetailReview.jsx
+++ src/Components/SelectHotelDetail/HotelDetailReview.jsx
@@ -9,7 +9,7 @@ const ReviewsComponent = ({ reviews }) => {
     <div className="bg-white rounded-lg p-6 shadow-md h-full">
       <div className="text-lg font-semibold mb-4 flex items-center justify-between ">
         <div>
-        Reviews
+          Reviews
         </div>
         <div className='bg-gradient-to-r from-[#f08a4b] to-[#e05b3c] p-2 rounded-lg text-white'>{averageRating}</div>
       </div>
